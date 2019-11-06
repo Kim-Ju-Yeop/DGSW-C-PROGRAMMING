a91b4a8c91d2f1c8bbd779e43de7746e024965af
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2019-11-06 11:18:48 +0900

    C-PROGRAMMING

---
+++ Example/[48] 수업 시간 예제 - Selection 정렬.c
@@ -0,0 +1,35 @@
+// 선택 정렬
+#include <stdio.h>
+
+void selection(int arr[], int size) {
+	
+	int i, j, temp, maxIdx;
+	int print;
+
+	for (i = 0; i < size - 1; i++) {
+			maxIdx = i;
+		
+		for (j = i + 1; j < size; j++) {
+			if (arr[j] < arr[maxIdx]) {
+				maxIdx = j;
+			}
+		}
+
+		temp = arr[i];
+		arr[i] = arr[maxIdx];
+		arr[maxIdx] = temp;
+
+		for (print = 0; print < size; print++) {
+			printf("%d ", arr[print]);
+		}
+		printf("\n");
+	}
+}
+
+
+int main() {
+	
+	int array[5] = { 3,2,4,5,1 };
+
+	selection(array, sizeof(array) / sizeof(int));
+}
