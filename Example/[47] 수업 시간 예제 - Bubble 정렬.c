68b52ad401eca285e2cc3d538ac31c20499de1a2
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2019-11-06 11:18:37 +0900

    C-PROGRAMMING

---
+++ Example/[47] 수업 시간 예제 - Bubble 정렬.c
@@ -0,0 +1,32 @@
+// 버블 정렬
+#include <stdio.h>
+
+void bubble(int arr[], int size) {
+	
+	int i, j, temp; 
+	int print;
+	
+	for (i = 0; i < size - 1; i++) {
+
+		for (j = 0; j < (size - i) - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+
+		for (print = 0; print < size; print++) {
+			printf("%d ", arr[print]);
+		}
+
+		printf("\n");
+	}
+}
+
+int main() {
+
+	int array[5] = { 3,2,4,5,1 };
+
+	bubble(array, sizeof(array) / sizeof(int));
+}
